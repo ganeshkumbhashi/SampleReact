e867435b64ddef988cc707d9886f40829f6244d5
ganesh.kumbhashi <ganesh.kumbhashi@candelalabs.io>
2018-07-29 16:17:17 +0530

    React Demo App

---
+++ src/App.js
@@ -0,0 +1,151 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+//let welcomeMsg = "Welcome to React JS 4";
+
+ const name = 'Ganesh';
+
+ const user = {
+   name,
+ };
+
+ const largeColumn = {
+   width:'40%',
+ };
+
+ const midColumn = {
+   width:'30%',
+ };
+
+ const smallColumn = {
+   width: '10%',
+ };
+
+
+
+let list = [
+  {
+    title: 'React',
+    url: 'https://react.com',
+    author: user.name,
+    num_comments: 3,
+    points: 10,
+    objectID: 2
+  },
+  {
+    title: 'Angular',
+    url: 'https://angular.com',
+    author: 'Suresh',
+    num_comments: 3,
+    points: 5,
+    objectID: 3
+  },
+  {
+    title: 'Vue',
+    url: 'https://vue.com',
+    author: 'Ramesh',
+    num_comments: 1,
+    points: 5,
+    objectID: 4
+  }
+];
+
+const isSearched = searchTerm => item => {
+ 
+  return item.title.toLowerCase().includes(searchTerm.toLowerCase());
+  
+};
+
+
+
+class App extends Component {
+
+  constructor(props)
+  {
+    super(props);
+    this.state = {
+      list,
+      searchTerm:'',
+    };
+
+    
+  }
+
+  onSearchChange = (event) => {
+    
+    this.setState({
+      searchTerm : event.target.value
+    });
+  }
+
+  
+
+  onDismiss = (id) => {
+    
+    const isNotId = item => item.objectID !== id;
+
+    const updatedList = this.state.list.filter(isNotId);
+
+    this.setState({list:updatedList});
+    
+  }
+
+  render() {
+    //var welcomeMsg = "Welcome to React JS";
+
+    const {list, searchTerm} = this.state;
+  
+  return (
+
+      <div className="page">
+        <div className="interactions">
+        <Search value={searchTerm} onChange={this.onSearchChange} >
+          Search
+        </Search>
+        </div>
+
+       
+
+        <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss}/>
+      </div>
+
+    );
+  }
+}
+
+
+class Search extends Component {
+  render()
+  {
+    const {value,onChange,children} = this.props;
+    return(
+      <form>
+            {children} <input type="text" value={value} onChange={onChange}/>
+      </form>
+    );
+  }
+}
+
+const Button = ({onClick,className='',children}) =>  <button onClick={onClick} className={className} type="button">{children}</button>;
+
+const Table = ({list,pattern,onDismiss}) => 
+  <div className="table">
+    {list.filter(isSearched(pattern)).map(item => 
+      <div key={item.objectID} class="table-row">
+        <span style={largeColumn}>
+            <a href={item.url}>{item.title}</a>
+        </span>
+        <span style={midColumn} >{item.author}</span>
+        <span style={smallColumn}>{item.num_comments}</span>
+        <span style={smallColumn}>{item.points}</span>
+        <span style={smallColumn}>
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline">Dismiss</Button>
+        </span>
+      </div>  
+    )} 
+  </div>
+
+
+
+export default App;
